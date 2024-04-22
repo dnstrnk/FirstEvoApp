8de6d32550b5af783f909060e2a4e08dd21182b9
dnstrnk <dnstrnk@gmail.com>
2024-04-22 15:40:42 +0300

    complete

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  counter = 0;
+  changeCount(type: boolean) {
+    type ? this.counter++ : this.counter--;
+  }
+}
