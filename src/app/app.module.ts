de5f631ccab074e76210b5464ff92172ab0c0d71
dnstrnk <dnstrnk@gmail.com>
2024-07-17 15:53:35 +0300

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { ItemComponent } from './item/item.component';
+import { ItemDetailsComponent } from './item/item-details/item-details.component';
+import { ItemStatsComponent } from './item/item-stats/item-stats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ContactComponent,
+    AboutComponent,
+    ItemComponent,
+    ItemDetailsComponent,
+    ItemStatsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/item/item-stats/item-stats.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-stats',
+  templateUrl: './item-stats.component.html',
+  styleUrls: ['./item-stats.component.css']
+})
+export class ItemStatsComponent {
+  constructor() {}
+
+  ngOnInit() {}
+}
