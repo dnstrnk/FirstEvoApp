3c7ea2074ddc48d6d11b896471bf06fbeab33c06
dnstrnk <dnstrnk@gmail.com>
2024-07-21 23:17:18 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FirstEvoApp';
+  dismissible = true;
+  defaultAlerts: any[] = [
+    {
+      type: 'success',
+      msg: `You successfully read this important alert message.`,
+    },
+    {
+      type: 'info',
+      msg: `This alert needs your attention, but it's not super important.`,
+    },
+    {
+      type: 'danger',
+      msg: `Better check yourself, you're not looking too good.`,
+    },
+  ];
+  alerts = this.defaultAlerts;
+
+  reset(): void {
+    this.alerts = this.defaultAlerts;
+  }
+
+  onClosed(dismissedAlert: any): void {
+    this.alerts = this.alerts.filter((alert) => alert !== dismissedAlert);
+  }
+  notiflix_alert(type: string) {
+    switch (type) {
+      case 'success':
+        Notify.success('Success Alert');
+        break;
+      case 'warning':
+        Notify.warning('Warning Alert');
+        break;
+      case 'error':
+        Notify.failure('Error Alert');
+        break;
+      case 'info':
+        Notify.info('Info Alert');
+        break;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AlertModule,
+    BrowserAnimationsModule,
+    TooltipModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  
+}
