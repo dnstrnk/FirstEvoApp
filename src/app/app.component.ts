74cd49fbcd8be69d263605043bb43d52906d8fbe
dnstrnk <dnstrnk@gmail.com>
2024-07-25 20:14:03 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
+import { Observable, Subscription, interval } from 'rxjs';
+import { CounterService } from './counter.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'FirstEvoApp';
+  constructor(public counterService: CounterService) {}
+  
+  counterSubs$!: Subscription;
+  enableStopButton = false;
+
+  getCounter() {
+    this.counterSubs$ = this.counterService.setCounter().subscribe((next) => {
+      console.log(next);
+    });
+    this.enableStopButton = true;
+  }
+
+  stopSubs() {
+    this.counterSubs$.unsubscribe();
+    this.enableStopButton = false
+  }
 }
