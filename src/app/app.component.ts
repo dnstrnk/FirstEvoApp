bca71f3534ccb7233701868206c5bbcf5c6e5962
dnstrnk <dnstrnk@gmail.com>
2024-07-28 13:49:27 +0300

    Revert "fix"
    This reverts commit 55757726ab737ece45e6fa9df1f1a16272dfc3f8.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,10 @@
-import { Component, OnInit, VERSION } from '@angular/core';
-import { Store } from '@ngxs/store';
-import { AuthState } from '../store/auth.state';
-import { AuthUpdate } from '../store/model/auth.model';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'FirstEvoApp';
-  constructor(private store: Store) {}
-
-  currentAuth = this.store.selectSnapshot(AuthState.getAuthObject);
-
-  ngOnInit() {
-    console.log(this.store.selectSnapshot(AuthState.getToken));
-    console.log(this.store.selectSnapshot(AuthState.getAuthObject));
-
-    this.store.select(AuthState.getAuthObject).subscribe({
-      next: (value) => {
-        this.currentAuth = value;
-      },
-    });
-  }
-
-  updateAuthState() {
-    this.store.dispatch(
-      new AuthUpdate({
-        isAuth: true,
-        login: 'loginName',
-        token: 'tokenString',
-      })
-    );
-  }
-
-  resetAuthState() {
-    this.store.dispatch(
-      new AuthUpdate({
-        isAuth: false,
-        login: null,
-        token: null,
-      })
-    );
-  }
 }
