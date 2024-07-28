55757726ab737ece45e6fa9df1f1a16272dfc3f8
dnstrnk <dnstrnk@gmail.com>
2024-07-28 13:00:05 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { AuthState } from '../store/auth.state';
+import { AuthUpdate } from '../store/model/auth.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'FirstEvoApp';
+  constructor(private store: Store) {}
+
+  currentAuth = this.store.selectSnapshot(AuthState.getAuthObject);
+
+  ngOnInit() {
+    console.log(this.store.selectSnapshot(AuthState.getToken));
+    console.log(this.store.selectSnapshot(AuthState.getAuthObject));
+
+    this.store.select(AuthState.getAuthObject).subscribe({
+      next: (value) => {
+        this.currentAuth = value;
+      },
+    });
+  }
+
+  updateAuthState() {
+    this.store.dispatch(
+      new AuthUpdate({
+        isAuth: true,
+        login: 'loginName',
+        token: 'tokenString',
+      })
+    );
+  }
+
+  resetAuthState() {
+    this.store.dispatch(
+      new AuthUpdate({
+        isAuth: false,
+        login: null,
+        token: null,
+      })
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { NgxsModule } from '@ngxs/store';
+import { AuthState } from '../store/auth.state';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    NgxsModule.forRoot([AuthState]),
+    NgxsLoggerPluginModule.forRoot(),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
