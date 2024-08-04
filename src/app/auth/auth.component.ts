50b95e7db37bd348e5f7a132498d643a9c26e9fb
dnstrnk <dnstrnk@gmail.com>
2024-08-04 19:46:49 +0300

    fix

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { DataService } from '../services/data.service';
+<<<<<<< Updated upstream
 import { FormsModule } from '@angular/forms';
 import { Store, Select } from '@ngxs/store';
 import { AuthState } from '../../store/auth.state';
 import { AuthUpdate } from '../../store/model/auth.model';
+=======
+>>>>>>> Stashed changes
 
 interface authValid {
   username: string | null;
@@ -15,6 +18,7 @@ interface authValid {
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent {
+<<<<<<< Updated upstream
   token!:string;
   data:authValid[] = [
   ]
@@ -46,4 +50,7 @@ export class AuthComponent {
       )}
     })
   }
+=======
+  constructor (private dataService:DataService) {}
+>>>>>>> Stashed changes
 }
